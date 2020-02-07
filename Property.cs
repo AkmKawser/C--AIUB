ed37a9bbe7ac5dc8e1990159c9ca2c2cbaa0c40b
Akm Kawser <kawserrabbi3@gmail.com>
2020-02-07 16:50:29 +0600

    Update Property.cs

--- Property.cs
+++ Property.cs
@@ -1,72 +1,79 @@
 //Instead of using MANUAL set get methods we can use property that gives built in SET GET METHOD FEATURES 
 
 
-using System;
-namespace ConsoleApplication1
+/*1st assembly name -ConsoleApplication [Always এই নাম দিব কারন এইটা mainly এটা Console Application file] - এটা Startup Porject*/
+//This assembly contains - Program.cs file 
+using Mynamespace;
+namespace ConsoleApp6
 {
-    public class Person
+    class Program
     {
-        private string name;
-        private int age;
-        private String dob;
-        private String address;
+        static void Main(string[] args)
+        {
+
+            Person p = new Person();
+
+            p.Name = "Kawser";
+            p.Age = 27;
+            p.Dob = "29th April";
+            p.Address = "Mirpur Dhaka";
+            p.ShowDetails();
+        }
+    }
+}
+
+
+
+
+/*2nd assembly name - ClassLibrary [Always এই নাম দিব কারন এইটা mainly এটা Class Library file] */
+//This assembly contains - Person.cs file
+using System;
+
+namespace Mynamespace
+{
+    
+        public class Person
+     {
+            private string name;
+            private int age;
+            private String dob;
+            private String address;
 
 
 
         public String Name
         {
-
-            set { this.name = value;}        
-            get { return this.name;}
-
+            set { this.name = value; }
+            get { return this.name; }
         }
-
-
 
         public int Age
         {
-            set { this.age = value;}
-            get { return this.age;}            
+            set { this.age = value; }
+            get { return this.age; }
         }
-      
 
 
         public String Dob
         {
             set { this.dob = value; }
-            get { return this.dob;}
-
+            get { return this.dob; }
         }
-       
+
 
         public String Address
         {
             set { this.address = value; }
             get { return this.address; }
-
         }
+
 
         public void ShowDetails()
         {
-            System.Console.WriteLine("Student Name is: " + name);
-            System.Console.WriteLine("Student Age is: " + age);
-            System.Console.WriteLine("Student DOB is: " + dob);
-            System.Console.WriteLine("Student Address is: " + address);
-
+            Console.WriteLine("Student Name is: " + name);
+            Console.WriteLine("Student Age is: " + age);
+            Console.WriteLine("Student DOB is: " + dob);
+            Console.WriteLine("Student Address is: " + address);
         }
-
-
-        static void Main(string[] args)
-        {
-            Person p = new Person();
-
-            p.Name = "Kawser";
-            p.Age = 27;
-            p.Dob = "29th aprial";
-            p.Address = "Mirpur Dhaka";
-            p.ShowDetails();
-
-        }
-
     }
 }
