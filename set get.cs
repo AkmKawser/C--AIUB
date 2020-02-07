a408af3fabea3b22434024735504cc4b8b708b86
Akm Kawser <kawserrabbi3@gmail.com>
2020-02-07 17:06:30 +0600

    Update set get.cs

--- set get.cs
+++ set get.cs
@@ -1,10 +1,5 @@
 /* To use private members in different class we use set get methods*/
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApplication1
 {
      public  class Person
