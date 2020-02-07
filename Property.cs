d483fbb44e681d437c96f1515754a4e5b935e1d5
Akm Kawser <kawserrabbi3@gmail.com>
2020-02-07 14:03:52 +0600

    Update Property.cs

--- Property.cs
+++ Property.cs
@@ -2,11 +2,6 @@
 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ConsoleApplication1
 {
     public class Person
