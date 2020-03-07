7c3c4db91e971e576bfa493985616423415b2c56
Akm Kawser <kawserrabbi3@gmail.com>
2020-03-08 00:45:08 +0600

    Update Lab Exam Mid Term.cs

--- Lab Exam Mid Term.cs
+++ Lab Exam Mid Term.cs
@@ -1,4 +1,4 @@
-//Class Name: OFFSIDECOURSE.cs
+//Class Name: OffsideCourse.cs
 //Namespace Name: Lab_Final_Library
 
 using System;
@@ -31,6 +31,153 @@ namespace Lab_Final_Library
             Console.Write("] ");
             Console.Write("Town=" + this.town+"\n");
         
+        }
+    }
+}
+
+//Class Name: CampusCourse.cs
+//Namespace Name: Lab_Final_Library
+
+
+using System;
+
+
+namespace Lab_Final_Library
+{
+    public class CampusCourse:Course
+    {
+        private string labNumber;
+
+        public CampusCourse() { 
+        
+        }
+
+        public string LabNumber
+        {
+            set { this.labNumber = value; }
+            get { return this.labNumber; }
+        }
+
+        public void Print()
+        {
+            Console.Write("CampusCourse: Name="+name+";");
+            Console.Write("Teacher= " + tCher.Name + ";");
+            Console.Write("Topics=[");
+            for (int i = 0; i < topics.Length; i++)
+            {
+                if (topics[i] != null)
+                {
+                    Console.Write("" + topics[i] + ",");
+                }
+
+            }
+            Console.Write("] ");
+            Console.Write("Lab=" + this.labNumber + "\n");
+
+        }
+
+    }
+}
+
+
+//Class Name: Class1.cs
+//Namespace Name: Lab_Final_Library
+
+using System;
+namespace Lab_Final_Library
+{
+    public class Course
+    {
+        protected string name;
+        protected Teacher tCher;
+        protected string[] topics;
+
+        public Course() { 
+         this.topics = new string[25];
+        }
+
+        public Course(string cName,Teacher t){
+            this.name=cName;
+            this.tCher=t;
+        
+        }
+
+        public Teacher Teacher {
+            set { this.tCher = value; }
+            get { return this.tCher; }
+        } 
+
+        public string Name{
+            set { this.name = value; }
+            get {return this.name;}
+        }
+
+
+
+        public void AddTopic(string topic) { 
+            for(int i=0;i < this.topics.Length;i++)
+            {
+                if(this.topics[i]==null)
+                {
+                    this.topics[i] = topic;
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
+
+
+/Class Name: Class2.cs
+//Namespace Name: Lab_Final_Library
+
+namespace Lab_Final_Library
+{
+    class Class2
+    {
+    }
+}
+
+
+
+/Class Name: Program.cs
+//Namespace Name: C_Sharp_lab_Final35381
+
+using System;
+using Lab_Final_Library;
+
+namespace C_Sharp_lab_Final35381
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CampusCourse c = new CampusCourse();
+            c.Name = "OOP2";
+            c.Teacher = new Teacher("Victor Stany Rozario");
+            c.LabNumber = "CL6";
+            c.AddTopic("into DB");
+            c.AddTopic("Property");
+            c.AddTopic("PaRams");
+            c.AddTopic("Arrray");
+            c.Print();
+            
+
+            OffSideCourse s = new OffSideCourse();
+            s.Name = "java";
+            s.Teacher = new Teacher("Victor Stany Rozario");
+            s.Town = "Dhaka";
+            s.AddTopic("gui");
+            s.AddTopic("AWT");
+            s.AddTopic("Inheritance");
+            s.Print();
+
+            Teacher t = new Teacher("Ehsanul haque", c, s);
+            t.Print();
+
+            Console.ReadKey();
         }
     }
 }
